2a6ff4cd106cceaa9e85e4f6df811b7be9666d05
Lana Shotashvili <shotashvililana@gmail.com>
2026-07-24 14:56:42 +0400

    feat: integrate GSAP for animations and add Tailwind CSS for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <h1 className="text-3xl text-indigo-300">App</h1>
     </>
   );
 }
