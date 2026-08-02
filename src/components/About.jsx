745dff21773b108af4bc96e6736899244989b5ce
Lana Shotashvili <shotashvililana@gmail.com>
2026-08-02 13:40:20 +0400

    feat: Add GSAP scroll animations to About component for enhanced visual effects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,39 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
 const About = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#about h2", { type: "words" });
+
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+      },
+    });
+
+    scrollTimeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        duration: 1,
+        yPercent: 100,
+        ease: "expo.out",
+        stagger: 0.02,
+      })
+      .from(
+        ".top-grid div, .bottom-grid div",
+        {
+          opacity: 0,
+          duration: 1,
+          ease: "power1.inOut",
+          stagger: 0.04,
+        },
+        "-=0.5"
+      );
+  }, []);
   return (
-    <div id="about">
+    <section id="about">
       <div className="mb:16 md:px-0 px-5">
         <div className="content">
           <div className="md:col-span-8">
@@ -61,7 +94,7 @@ const About = () => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
