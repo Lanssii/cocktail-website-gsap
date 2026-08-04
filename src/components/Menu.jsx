17d7d5d1ba101910fa639e15c438584928703ce2
Lana Shotashvili <shotashvililana@gmail.com>
2026-08-04 13:25:41 +0400

    feat(menu): implement infinite carousel logic and dynamic cocktail navigation
    - Added `getCocktailAt` helper function to safely calculate cyclic array indices using the remainder operator (%).
    - Introduced `currentCocktail`, `prevCocktail`, and `nextCocktail` variables based on the current slide index.
    - Updated arrow buttons to dynamically render the names of the previous and next cocktails.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -14,6 +14,16 @@ const Menu = () => {
     setCurrentIndex(newIndex);
   };
 
+  const getCocktailAt = (indexOffSet) => {
+    return allCocktails[
+      (currentIndex + indexOffSet + totalCocktails) % totalCocktails
+    ];
+  }; // we now which one is current one, previous or next. This mathematical formula protects code from going beyond the bounds of an array.
+
+  const currentCocktail = getCocktailAt(0);
+  const prevCocktail = getCocktailAt(-1);
+  const nextCocktail = getCocktailAt(1);
+
   return (
     <section id="menu" aria-label="menu-heading">
       <img
@@ -51,6 +61,38 @@ const Menu = () => {
           );
         })}
       </nav>
+
+      <div className="content">
+        <div className="arrows">
+          <button
+            className="text-left"
+            onClick={() => goToSlide(currentIndex - 1)}
+          >
+            <span>{prevCocktail.name}</span>
+            <img
+              src="/images/right-arrow.png"
+              alt="right-arrow"
+              aria-hidden="true"
+            />
+          </button>
+
+          <button
+            className="text-right"
+            onClick={() => goToSlide(currentIndex + 1)}
+          >
+            <span>{nextCocktail.name}</span>
+            <img
+              src="/images/left-arrow.png"
+              alt="left-arrow"
+              aria-hidden="true"
+            />
+          </button>
+        </div>
+
+        <div className="cocktail">
+          <img src={currentCocktail.image} alt="" className="object-contain" />
+        </div>
+      </div>
     </section>
   );
 };
